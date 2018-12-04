5db5e476d47db787beac8d83bc14a887ee2aefb0
Seth Martin Ely <sethely@Seths-MBPp.home>
2018-12-04 10:55:17 -0600

    changed 'npm run build' from 'npm build'

--- index.js
+++ index.js
@@ -75,6 +75,6 @@ app.get('/me', (req, res, next) => {
 });
 
 
-app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname+'/client/public/index.html'))
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname+'/client/build/index.html'))
 })
